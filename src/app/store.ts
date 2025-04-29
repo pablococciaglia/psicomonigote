978db835532b279c8e9705ad0c2520cf5b59d43c
Pablo Cocciaglia <cocciaglia@gmail.com>
2025-04-29 03:41:22 +0200

    remove unnecessary files and commentsw

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,37 +3,24 @@ import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import { modalSlice } from "./modalSlice"
 import { authSlice } from "./slices/authSlice"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 
-const rootReducer = combineSlices(
-  counterSlice,
-  quotesApiSlice,
-  modalSlice,
-  authSlice,
-)
+const rootReducer = combineSlices(modalSlice, authSlice)
 
 export type RootState = ReturnType<typeof rootReducer>
 
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
-    },
     preloadedState,
   })
-  // configure listeners using the provided defaults
-  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+
   setupListeners(store.dispatch)
   return store
 }
 
 export const store = makeStore()
 
-// Infer the type of `store`
 export type AppStore = typeof store
-// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore["dispatch"]
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
